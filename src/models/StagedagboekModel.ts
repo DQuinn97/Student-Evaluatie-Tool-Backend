d6cf950cec41a3752731f08c2df3bdb39618ee32
DQuinn97 <quinten2407@gmail.com>
2025-04-01 13:52:33 +0200

    stagedagboek model

---
+++ src/models/StagedagboekModel.ts
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+import { Gebruiker } from "./GebruikerModel";
+import { Klasgroep } from "./KlasgroepModel";
+import { Stageverslag } from "./StageverslagModel";
+import { Stagedag } from "./StagedagModel";
+
+const stagedagboekSchema = new mongoose.Schema(
+  {
+    student: {
+      type: Schema.Types.ObjectId,
+      ref: Gebruiker,
+      required: true,
+      trim: true,
+    },
+    klasgroep: {
+      type: Schema.Types.ObjectId,
+      ref: Klasgroep,
+      required: true,
+      trim: true,
+    },
+    stageverslag: {
+      type: Schema.Types.ObjectId,
+      ref: Stageverslag,
+      nullable: true,
+    },
+    stagedagen: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: Stagedag,
+        trim: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Stagedagboek = mongoose.model("Stagedagboek", stagedagboekSchema);
