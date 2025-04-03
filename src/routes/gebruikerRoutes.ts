1b030f4d6ac755eac3baa358fdac0a3a652f3001
DQuinn97 <quinten2407@gmail.com>
2025-04-03 09:18:12 +0200

    gsm ipv tel

--- src/routes/gebruikerRoutes.ts
+++ src/routes/gebruikerRoutes.ts
@@ -39,7 +39,7 @@ const router = express.Router();
  *                 type: string
  *               achternaam:
  *                 type: string
- *               tel:
+ *               gsm:
  *                 type: string
  *     responses:
  *       '200':
