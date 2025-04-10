4e8b810c246be925054a83b63d92bd52bb27d6bc
DQuinn97 <quinten2407@gmail.com>
2025-04-10 16:16:08 +0200

    swaggerdocs versie 1.0.8

--- src/swagger.ts
+++ src/swagger.ts
@@ -6,7 +6,7 @@ const options = {
     openapi: "3.0.0",
     info: {
       title: "Student Evaluatie Tool API",
-      version: "1.0.7",
+      version: "1.0.8",
       description: "API documentatie voor Student Evaluatie Tool",
     },
     servers: isProduction
